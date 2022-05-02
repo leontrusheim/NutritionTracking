824237fa7615448dfa09a804141115a3d58ba417
leontrusheim <leon.trusheim@gmail.com>
2022-05-01 21:05:38 -0700

    Graphs are now implemented.

--- app/src/main/java/com/example/nutritiontracking/DailyGraphFragment.java
+++ app/src/main/java/com/example/nutritiontracking/DailyGraphFragment.java
@@ -9,7 +9,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
 public class DailyGraphFragment extends Fragment {
+
+    // Create the object of TextView
+    // and Piechart class
+    TextView tvProtein, tvFat, tvCarbs, tvJava;
+    PieChart pieChart;
 
 
     public DailyGraphFragment() {
@@ -19,16 +31,55 @@ public class DailyGraphFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
         Fragment dateFrag = new DateSelectorFragment();
         FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.dateBar, dateFrag);
         fragmentTransaction.commit();
+        */
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_daily_graph, container, false);
+        tvProtein = v.findViewById(R.id.tvProtein);
+        tvFat = v.findViewById(R.id.tvFat);
+        tvCarbs = v.findViewById(R.id.tvCarbs);
+        pieChart = v.findViewById(R.id.piechart);
+        // Creating a method setData()
+        // to set the text in text view and pie chart
+        setData();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_graph, container, false);
+        return v;
+    }
+
+    private void setData()
+    {
+
+        // Set the percentages
+        tvProtein.setText(Integer.toString(40));
+        tvFat.setText(Integer.toString(30));
+        tvCarbs.setText(Integer.toString(5));
+
+        // Set the data and color to the pie chart
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Protein",
+                        Integer.parseInt(tvProtein.getText().toString()),
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Fat",
+                        Integer.parseInt(tvFat.getText().toString()),
+                        Color.parseColor("#66BB6A")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Carbs",
+                        Integer.parseInt(tvCarbs.getText().toString()),
+                        Color.parseColor("#EF5350")));
+
+        // To animate the pie chart
+        pieChart.startAnimation();
     }
 }
--- app/src/main/java/com/example/nutritiontracking/WeeklyGraphFragment.java
+++ app/src/main/java/com/example/nutritiontracking/WeeklyGraphFragment.java
@@ -9,8 +9,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class WeeklyGraphFragment extends Fragment {
+
+    BarChart barChart;
+    BarData barData;
+    BarDataSet barDataSet;
+    ArrayList barEntriesArrayList;
+    int targetValue = 6;
+    private static final String[] DAYS = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
 
 
     public WeeklyGraphFragment() {
@@ -26,11 +48,47 @@ public class WeeklyGraphFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v=  inflater.inflate(R.layout.fragment_weekly_graph, container, false);
+        View v = inflater.inflate(R.layout.fragment_weekly_graph, container, false);
+
+        /*
         String[] categories = getResources().getStringArray(R.array.categories);
         ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), R.layout.dropdown_menu_item, categories);
         Spinner sp = v.findViewById(R.id.spinner);
         sp.setAdapter(arrayAdapter);
+         */
+        barChart = v.findViewById(R.id.idBarChart);
+        barEntriesArrayList = new ArrayList<>();
+        barEntriesArrayList.add(new BarEntry(0f, 4));
+        barEntriesArrayList.add(new BarEntry(1f, 6));
+        barEntriesArrayList.add(new BarEntry(2f, 8));
+        barEntriesArrayList.add(new BarEntry(3f, 2));
+        barEntriesArrayList.add(new BarEntry(4f, 4));
+        barEntriesArrayList.add(new BarEntry(5f, 1));
+        barEntriesArrayList.add(new BarEntry(6f, 1));
+        barDataSet = new BarDataSet(barEntriesArrayList, "Weekly Nutrients");
+        configureChartAppearance();
+        barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        int maxCapacity = 100;
+        LimitLine ll = new LimitLine(targetValue, "Target Value");
+        barChart.getAxisLeft().addLimitLine(ll);
+        barDataSet.setColors(getResources().getColor(R.color.green_dark));
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(false);
         return v;
+    }
+
+    private void configureChartAppearance() {
+        barChart.getDescription().setEnabled(false);
+        barChart.setDrawValueAboveBar(false);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return DAYS[(int) value];
+            }
+        });
     }
 }
